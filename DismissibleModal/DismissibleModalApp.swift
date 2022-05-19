32696d884fc8105281095b0f206770fa5f403151
Joshua Shroyer <jwshroyer@users.noreply.github.com>
2022-05-19 19:04:02 -0400

    Initial commit

---
+++ DismissibleModal/DismissibleModalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DismissibleModalApp.swift
+//  DismissibleModal
+//
+//  Created by Joshua Shroyer on 5/19/22.
+//
+
+import SwiftUI
+
+@main
+struct DismissibleModalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
